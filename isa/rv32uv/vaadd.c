873a6be6580cb1380db20b725871a1ce4904259a
Daniel Schloms <daniel.schloms@gmail.com>
2024-10-11 23:03:33 +0200

    WIP: Update averaging tests with different rounding modes

--- isa/rv32uv/vaadd.c
+++ isa/rv32uv/vaadd.c
@@ -7,7 +7,51 @@
 
 #include "vector_macros.h"
 
+// Rounding mode rnu
 void TEST_CASE1(void) {
+  __asm__ volatile("csrwi vxrm, 0");
+  VSET(4, e8, m1);
+  VLOAD_8(v1, 1, -2, -3, 4);
+  VLOAD_8(v2, 1, 3, -3, 4);
+  __asm__ volatile("vaadd.vv v3, v1, v2" ::);
+  VCMP_I8(1, v3, 1, 1, -3, 4);
+}
+
+void TEST_CASE2(void) {
+  __asm__ volatile("csrwi vxrm, 0");
+  VSET(4, e8, m1);
+  VLOAD_8(v1, 1, -2, -3, 4);
+  VLOAD_8(v2, 1, 3, -3, 4);
+  VLOAD_8(v0, 0xA, 0x0, 0x0, 0x0);
+  VCLEAR(v3);
+  __asm__ volatile("vaadd.vv v3, v1, v2, v0.t" ::);
+  VCMP_I8(2, v3, 0, 1, 0, 4);
+}
+
+void TEST_CASE3(void) {
+  __asm__ volatile("csrwi vxrm, 0");
+  VSET(4, e32, m1);
+  VLOAD_32(v1, 1, -2, 4, -4);
+  const uint32_t scalar = 5;
+  __asm__ volatile("vaadd.vx v3, v1, %[A]" ::[A] "r"(scalar));
+  VCMP_I32(3, v3, 3, 2, 5, 1);
+}
+
+// Dont use VCLEAR here, it results in a glitch where are values are off by 1
+void TEST_CASE4(void) {
+  __asm__ volatile("csrwi vxrm, 0");
+  VSET(4, e32, m1);
+  VLOAD_32(v1, 1, -2, 3, -4);
+  const uint32_t scalar = 5;
+  VLOAD_32(v0, 0xA, 0x0, 0x0, 0x0);
+  VCLEAR(v3);
+  __asm__ volatile("vaadd.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
+  VCMP_I32(4, v3, 0, 2, 0, 1);
+}
+
+// Rounding mode rne
+void TEST_CASE5(void) {
+  __asm__ volatile("csrwi vxrm, 1");
   VSET(4, e8, m1);
   VLOAD_8(v1, 1, -2, -3, 4);
   VLOAD_8(v2, 1, 2, -3, 4);
@@ -15,7 +59,8 @@ void TEST_CASE1(void) {
   VCMP_I8(1, v3, 1, 0, -3, 4);
 }
 
-void TEST_CASE2(void) {
+void TEST_CASE6(void) {
+  __asm__ volatile("csrwi vxrm, 1");
   VSET(4, e8, m1);
   VLOAD_8(v1, 1, -2, -3, 4);
   VLOAD_8(v2, 1, 2, -3, 4);
@@ -25,7 +70,8 @@ void TEST_CASE2(void) {
   VCMP_I8(2, v3, 0, 0, 0, 4);
 }
 
-void TEST_CASE3(void) {
+void TEST_CASE7(void) {
+  __asm__ volatile("csrwi vxrm, 1");
   VSET(4, e32, m1);
   VLOAD_32(v1, 1, -2, 3, -4);
   const uint32_t scalar = 5;
@@ -34,7 +80,92 @@ void TEST_CASE3(void) {
 }
 
 // Dont use VCLEAR here, it results in a glitch where are values are off by 1
-void TEST_CASE4(void) {
+void TEST_CASE8(void) {
+  __asm__ volatile("csrwi vxrm, 1");
+  VSET(4, e32, m1);
+  VLOAD_32(v1, 1, -2, 3, -4);
+  const uint32_t scalar = 5;
+  VLOAD_32(v0, 0xA, 0x0, 0x0, 0x0);
+  VCLEAR(v3);
+  __asm__ volatile("vaadd.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
+  VCMP_I32(4, v3, 0, 2, 0, 1);
+}
+
+// Rounding mode rdn
+void TEST_CASE9(void) {
+  __asm__ volatile("csrwi vxrm, 2");
+  VSET(4, e8, m1);
+  VLOAD_8(v1, 1, -2, -3, 4);
+  VLOAD_8(v2, 1, 2, -3, 4);
+  __asm__ volatile("vaadd.vv v3, v1, v2" ::);
+  VCMP_I8(1, v3, 1, 0, -3, 4);
+}
+
+void TEST_CASE10(void) {
+  __asm__ volatile("csrwi vxrm, 2");
+  VSET(4, e8, m1);
+  VLOAD_8(v1, 1, -2, -3, 4);
+  VLOAD_8(v2, 1, 2, -3, 4);
+  VLOAD_8(v0, 0xA, 0x0, 0x0, 0x0);
+  VCLEAR(v3);
+  __asm__ volatile("vaadd.vv v3, v1, v2, v0.t" ::);
+  VCMP_I8(2, v3, 0, 0, 0, 4);
+}
+
+void TEST_CASE1(void) {
+  __asm__ volatile("csrwi vxrm, 2");
+  VSET(4, e32, m1);
+  VLOAD_32(v1, 1, -2, 3, -4);
+  const uint32_t scalar = 5;
+  __asm__ volatile("vaadd.vx v3, v1, %[A]" ::[A] "r"(scalar));
+  VCMP_I32(3, v3, 3, 2, 4, 1);
+}
+
+// Dont use VCLEAR here, it results in a glitch where are values are off by 1
+void TEST_CASE12(void) {
+  __asm__ volatile("csrwi vxrm, 2");
+  VSET(4, e32, m1);
+  VLOAD_32(v1, 1, -2, 3, -4);
+  const uint32_t scalar = 5;
+  VLOAD_32(v0, 0xA, 0x0, 0x0, 0x0);
+  VCLEAR(v3);
+  __asm__ volatile("vaadd.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
+  VCMP_I32(4, v3, 0, 2, 0, 1);
+}
+
+// Rounding mode rod
+void TEST_CASE13(void) {
+  __asm__ volatile("csrwi vxrm, 3");
+  VSET(4, e8, m1);
+  VLOAD_8(v1, 1, -2, -3, 4);
+  VLOAD_8(v2, 1, 2, -3, 4);
+  __asm__ volatile("vaadd.vv v3, v1, v2" ::);
+  VCMP_I8(1, v3, 1, 0, -3, 4);
+}
+
+void TEST_CASE14(void) {
+  __asm__ volatile("csrwi vxrm, 3");
+  VSET(4, e8, m1);
+  VLOAD_8(v1, 1, -2, -3, 4);
+  VLOAD_8(v2, 1, 2, -3, 4);
+  VLOAD_8(v0, 0xA, 0x0, 0x0, 0x0);
+  VCLEAR(v3);
+  __asm__ volatile("vaadd.vv v3, v1, v2, v0.t" ::);
+  VCMP_I8(2, v3, 0, 0, 0, 4);
+}
+
+void TEST_CASE15(void) {
+  __asm__ volatile("csrwi vxrm, 3");
+  VSET(4, e32, m1);
+  VLOAD_32(v1, 1, -2, 3, -4);
+  const uint32_t scalar = 5;
+  __asm__ volatile("vaadd.vx v3, v1, %[A]" ::[A] "r"(scalar));
+  VCMP_I32(3, v3, 3, 2, 4, 1);
+}
+
+// Dont use VCLEAR here, it results in a glitch where are values are off by 1
+void TEST_CASE16(void) {
+  __asm__ volatile("csrwi vxrm, 3");
   VSET(4, e32, m1);
   VLOAD_32(v1, 1, -2, 3, -4);
   const uint32_t scalar = 5;
