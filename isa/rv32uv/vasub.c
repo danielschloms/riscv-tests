b4b8051cd72cede90a14a12cbe99db134eb32997
Daniel Schloms <daniel.schloms@gmail.com>
2024-10-11 10:39:01 +0000

    Update some V tests

--- isa/rv32uv/vasub.c
+++ isa/rv32uv/vasub.c
@@ -12,7 +12,7 @@ void TEST_CASE1(void) {
   VLOAD_32(v1, 5, 10, 15, 20);
   VLOAD_32(v2, -1, 2, -3, 4);
   __asm__ volatile("vasub.vv v3, v1, v2" ::);
-  VEC_CMP_32(1, v3, 3, 4, 9, 8);
+  VCMP_I32(1, v3, 3, 4, 9, 8);
 }
 
 void TEST_CASE2(void) {
@@ -20,27 +20,27 @@ void TEST_CASE2(void) {
   VLOAD_32(v1, 5, 10, 15, 20);
   VLOAD_32(v2, 1, 2, 3, -4);
   VLOAD_32(v0, 10, 0, 0, 0);
-  CLEAR(v3);
+  VCLEAR(v3);
   __asm__ volatile("vasub.vv v3, v1, v2, v0.t" ::);
-  VEC_CMP_32(2, v3, 0, 4, 0, 12);
+  VCMP_I32(2, v3, 0, 4, 0, 12);
 }
 
 void TEST_CASE3(void) {
   VSET(4, e32, m1);
-  VLOAD_U32(v1, 5, 10, 15, 20);
+  VLOAD_32(v1, 5, 10, 15, 20);
   const uint64_t scalar = -5;
   __asm__ volatile("vasub.vx v3, v1, %[A]" ::[A] "r"(scalar));
-  VEC_CMP_U32(3, v3, 5, 8, 10, 13);
+  VCMP_U32(3, v3, 5, 8, 10, 13);
 }
 
 void TEST_CASE4(void) {
   VSET(4, e32, m1);
-  VLOAD_U32(v1, 5, 10, 15, 20);
+  VLOAD_32(v1, 5, 10, 15, 20);
   const uint64_t scalar = -5;
-  VLOAD_U32(v0, 10, 0, 0, 0);
-  CLEAR(v3);
+  VLOAD_32(v0, 10, 0, 0, 0);
+  VCLEAR(v3);
   __asm__ volatile("vasub.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
-  VEC_CMP_U32(4, v3, 0, 8, 0, 13);
+  VCMP_U32(4, v3, 0, 8, 0, 13);
 }
 
 int main(void) {
--- isa/rv32uv/vasubu.c
+++ isa/rv32uv/vasubu.c
@@ -9,38 +9,38 @@
 
 void TEST_CASE1(void) {
   VSET(4, e32, m1);
-  VLOAD_U32(v1, 5, 10, 15, 20);
-  VLOAD_U32(v2, 1, 2, 3, 4);
+  VLOAD_32(v1, 5, 10, 15, 20);
+  VLOAD_32(v2, 1, 2, 3, 4);
   __asm__ volatile("vasubu.vv v3, v1, v2" ::);
-  VEC_CMP_U32(1, v3, 2, 4, 6, 8);
+  VCMP_U32(1, v3, 2, 4, 6, 8);
 }
 
 void TEST_CASE2(void) {
   VSET(4, e32, m1);
-  VLOAD_U32(v1, 5, 10, 15, 20);
-  VLOAD_U32(v2, 1, 2, 3, 4);
-  VLOAD_U32(v0, 10, 0, 0, 0);
-  CLEAR(v3);
+  VLOAD_32(v1, 5, 10, 15, 20);
+  VLOAD_32(v2, 1, 2, 3, 4);
+  VLOAD_32(v0, 10, 0, 0, 0);
+  VCLEAR(v3);
   __asm__ volatile("vasubu.vv v3, v1, v2, v0.t" ::);
-  VEC_CMP_U32(2, v3, 0, 4, 0, 8);
+  VCMP_U32(2, v3, 0, 4, 0, 8);
 }
 
 void TEST_CASE3(void) {
   VSET(4, e32, m1);
-  VLOAD_U32(v1, 5, 10, 15, 20);
+  VLOAD_32(v1, 5, 10, 15, 20);
   const uint64_t scalar = 5;
   __asm__ volatile("vasubu.vx v3, v1, %[A]" ::[A] "r"(scalar));
-  VEC_CMP_U32(3, v3, 0, 3, 5, 8);
+  VCMP_U32(3, v3, 0, 3, 5, 8);
 }
 
 void TEST_CASE4(void) {
   VSET(4, e32, m1);
-  VLOAD_U32(v1, 5, 10, 15, 20);
+  VLOAD_32(v1, 5, 10, 15, 20);
   const uint64_t scalar = 5;
-  VLOAD_U32(v0, 10, 0, 0, 0);
-  CLEAR(v3);
+  VLOAD_32(v0, 10, 0, 0, 0);
+  VCLEAR(v3);
   __asm__ volatile("vasubu.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
-  VEC_CMP_U32(4, v3, 0, 3, 0, 8);
+  VCMP_U32(4, v3, 0, 3, 0, 8);
 }
 
 int main(void) {
