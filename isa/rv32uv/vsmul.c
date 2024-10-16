6fef7dfef8a662a795aa048bca9f5bae3f954d22
Daniel Schloms <daniel.schloms@gmail.com>
2024-10-16 14:15:55 +0000

    Update vsmul test

--- isa/rv32uv/vsmul.c
+++ isa/rv32uv/vsmul.c
@@ -8,14 +8,24 @@
 #include "vector_macros.h"
 
 void TEST_CASE1() {
+  WRITE_CSR_IMM(0, vxrm);
   VSET(3, e8, m1);
   VLOAD_8(v2, 127, 127, -50);
   VLOAD_8(v3, 127, 10, 127);
   __asm__ volatile("vsmul.vv v1, v2, v3");
   VCMP_I8(1, v1, 126, 10, -50);
+
+  // No saturation: vxsat must be 0
+  uint32_t vxsat;
+  READ_CSR(vxsat, vxsat);
+  if(vxsat){
+    num_failed++;
+  }
+  WRITE_CSR_IMM(0, vxsat);
 }
 
 void TEST_CASE2() {
+  WRITE_CSR_IMM(0, vxrm);
   VSET(3, e8, m1);
   VLOAD_8(v2, 127, 127, -50);
   VLOAD_8(v3, 127, 10, 127);
@@ -23,17 +33,35 @@ void TEST_CASE2() {
   VCLEAR(v1);
   __asm__ volatile("vsmul.vv v1, v2, v3, v0.t");
   VCMP_I8(2, v1, 126, 0, -50);
+
+  // No saturation: vxsat must be 0
+  uint32_t vxsat;
+  READ_CSR(vxsat, vxsat);
+  if(vxsat){
+    num_failed++;
+  }
+  WRITE_CSR_IMM(0, vxsat);
 }
 
 void TEST_CASE3() {
+  WRITE_CSR_IMM(0, vxrm);
   VSET(3, e8, m1);
   VLOAD_8(v2, 127, 63, -50);
   int8_t scalar = 55;
   __asm__ volatile("vsmul.vx v1, v2, %[A]" ::[A] "r"(scalar));
   VCMP_I8(3, v1, 55, 27, -21);
+
+  // No saturation: vxsat must be 0
+  uint32_t vxsat;
+  READ_CSR(vxsat, vxsat);
+  if(vxsat){
+    num_failed++;
+  }
+  WRITE_CSR_IMM(0, vxsat);
 }
 
 void TEST_CASE4() {
+  WRITE_CSR_IMM(0, vxrm);
   VSET(3, e8, m1);
   VLOAD_8(v2, 127, 127, -50);
   int8_t scalar = 55;
@@ -41,7 +69,34 @@ void TEST_CASE4() {
   VCLEAR(v1);
   __asm__ volatile("vsmul.vx v1, v2, %[A], v0.t" ::[A] "r"(scalar));
   VCMP_I8(4, v1, 55, 0, -21);
+
+  // No saturation: vxsat must be 0
+  uint32_t vxsat;
+  READ_CSR(vxsat, vxsat);
+  if(vxsat){
+    num_failed++;
+  }
+  WRITE_CSR_IMM(0, vxsat);
 }
+
+void TEST_CASE5() {
+  WRITE_CSR_IMM(0, vxrm);
+  VSET(3, e8, m1);
+  VLOAD_8(v2, -128, 127, -50);
+  VLOAD_8(v3, -128, 10, 127);
+  __asm__ volatile("vsmul.vv v1, v2, v3");
+  VCMP_I8(1, v1, 126, 10, -50);
+
+  // Saturation: vxsat must be 1
+  uint32_t vxsat;
+  READ_CSR(vxsat, vxsat);
+  if(!vxsat){
+    num_failed++;
+  }
+  WRITE_CSR_IMM(0, vxsat);
+}
+
+// TODO: test other rounding modes
 
 int main(void) {
   INIT_CHECK();
@@ -51,5 +106,6 @@ int main(void) {
   TEST_CASE2();
   TEST_CASE3();
   TEST_CASE4();
+  TEST_CASE5();
   EXIT_CHECK();
 }
