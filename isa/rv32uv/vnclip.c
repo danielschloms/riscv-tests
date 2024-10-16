ad15924e1aa1374bb2533d706afee51c4c804746
Daniel Schloms <daniel.schloms@gmail.com>
2024-10-16 14:16:15 +0000

    Add empty vnclip(u) tests

---
+++ isa/rv32uv/vnclip.c
@@ -0,0 +1,22 @@
+// Copyright 2021 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+//
+// Author: Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
+//         Basile Bougenot <bbougenot@student.ethz.ch>
+
+#include "vector_macros.h"
+
+void TEST_CASE1(void) {
+  num_failed++;
+};
+
+int main(void) {
+  INIT_CHECK();
+  enable_vec();
+  enable_fp();
+  
+  TEST_CASE1();
+
+  EXIT_CHECK();
+}
