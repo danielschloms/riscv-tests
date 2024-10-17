02131407d4a5872912411b1d4ee512442c9bb0d0
Daniel Schloms <daniel.schloms@gmail.com>
2024-10-17 15:23:25 +0000

    Fix scaling right shift tests

--- isa/rv32uv/vsmul.c
+++ isa/rv32uv/vsmul.c
@@ -85,7 +85,7 @@ void TEST_CASE5() {
   VLOAD_8(v2, -128, 127, -50);
   VLOAD_8(v3, -128, 10, 127);
   __asm__ volatile("vsmul.vv v1, v2, v3");
-  VCMP_I8(1, v1, 126, 10, -50);
+  VCMP_I8(1, v1, 127, 10, -50);
 
   // Saturation: vxsat must be 1
   uint32_t vxsat;
